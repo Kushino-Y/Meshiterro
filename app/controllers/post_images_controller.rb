ccaf42ba46fa342e4db37ffaf7d3221d90fa334f
Kushino-Y <kuskus7856.yi@gmail.com>
2021-03-18 01:55:07 +0000

    [Start]6-3

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,19 @@
+class PostImagesController < ApplicationController
+  
+  def new #画像の投稿画面を表示する
+  end
+  
+  def create #投稿画像を保存する
+  end
+  
+  def index #投稿画像のリストを表示する
+  end
+  
+  def show #投稿画像の詳細を表示する
+  end
+  
+  def destroy #投稿画像を削除する
+  end
+  
+  
+end
