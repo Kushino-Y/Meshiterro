eb56f1fffbc99b3b518f163d430d681f02c1d863
Kushino-Y <kuskus7856.yi@gmail.com>
2021-03-18 06:20:26 +0000

    [Finish]6-3

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,19 +1,34 @@
 class PostImagesController < ApplicationController
   
   def new #画像の投稿画面を表示する
+    @post_image =PostImage.new
   end
   
   def create #投稿画像を保存する
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
   
   def index #投稿画像のリストを表示する
+    @post_images = PostImage.all
   end
   
   def show #投稿画像の詳細を表示する
+    @post_image = PostImage.find(params[:id])
   end
   
   def destroy #投稿画像を削除する
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
   
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
   
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a1cc28ebd9907ab40f0bb4da95297ee1c4368d657ab1f72fe886f554651421dd261b53502deb76b6980846f250dcf021621df42622e0fd496ef58539541e69e5'
